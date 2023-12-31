ba646dfd043b1fa9a41656170160e106e620ce71
Faltrenn <emanuel.alves.702@ufrn.edu.br>
2023-12-30 23:43:43 -0300

    Adição dos controles de pc(teclado e mouse). Adaptação da skill de dash para teclado e mouse.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,14 +12,19 @@ var move_input: Vector2 = Vector2.RIGHT
 
 var can_move: bool = true
 
+var auto_walk_pos: Vector2 = Vector2.ZERO
+var auto_walk_dir: Vector2 = Vector2.ZERO
+
 func _ready():
 	init(100, 700)
 	
 	for i in range(0,4):
 		skills[i].set_script(char_res.skills[i])
 
-func _process(_delta):
-	look_input = Input.get_vector("look_left", "look_right", "look_up", "look_down").normalized()
+func _process(_delta: float):
+	var g_look = Input.get_vector("look_left", "look_right", "look_up", "look_down").normalized()
+	if g_look:
+		look_input = g_look
 	
 	for i in range(0,4):
 		if Input.is_action_just_pressed("skill_" + str(i+1)):
@@ -27,10 +32,26 @@ func _process(_delta):
 			if skill:
 				skill.execute(self)
 	fps_counter.text = "FPS: " + str(Engine.get_frames_per_second())
-
-func _physics_process(_delta):
-	move_input = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-	if can_move:
-		velocity = move_input * speed
 	
+	if auto_walk_dir > Vector2.ZERO and position >= auto_walk_pos or auto_walk_dir < Vector2.ZERO and position <= auto_walk_pos:
+		reset_auto_walk()
+
+func _input(event: InputEvent):
+	if event as InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+			auto_walk_pos = event.position
+			auto_walk_dir = (event.position - position).normalized()
+	elif event as InputEventMouseMotion:
+		look_input = (event.position - position).normalized()
+
+func reset_auto_walk():
+	auto_walk_dir = Vector2.ZERO
+	auto_walk_pos = Vector2.ZERO
+
+func _physics_process(_delta: float):
+	move_input = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	if move_input:
+		reset_auto_walk()
+	if can_move:
+		velocity = (move_input if move_input else auto_walk_dir) * speed
 	move_and_slide()
--- Scripts/Skills/Char1/Char1Skill3.gd
+++ Scripts/Skills/Char1/Char1Skill3.gd
@@ -7,8 +7,10 @@ func _init():
 	super._init()
 
 func execute(p_player: Player):
-	if not in_cooldown and p_player.move_input:
+	var direction = p_player.move_input if p_player.move_input else p_player.look_input
+	if not in_cooldown and direction:
 		var d = dash.new()
 		p_player.add_child(d)
-		d.init(p_player, 500, p_player.move_input, p_player.speed * 2)
+		d.init(p_player, 500, direction, p_player.speed * 2)
+		p_player.reset_auto_walk()
 		super.execute(p_player)
